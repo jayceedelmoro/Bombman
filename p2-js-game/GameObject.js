06a58fcbe34b1855b5ed3c064707aa9167f4b883
Carlo <jancarlodelmoro@gmail.com>
2023-02-19 16:02:14 +0800

    Feat: Added Person as Game Object

--- p2-js-game/GameObject.js
+++ p2-js-game/GameObject.js
@@ -2,6 +2,7 @@ class GameObjects {
     constructor(config) {
         this.locationX = config.locationX || 0;
         this.locationY = config.locationY || 0;
+        this.direction = config.direction || 'down';
         this.sprite = new Sprite({
             gameObject: this,
             src: config.src || 'assets/character/people/DemoRpgCharacter.png',
@@ -9,6 +10,28 @@ class GameObjects {
     }
 }
 
-class Persone extends GameObjects {
-    
+class Person extends GameObjects {
+    constructor(config) {
+        super(config);
+        this.remainingMovement = 16;
+
+        this.directionMovement = {
+            'up': ['locationY', -1],
+            'down': ['locationY', 1],
+            'left': ['locationX', -1],
+            'right': ['locationX', 1]
+        }
+    }
+
+    update(state) {
+        this.updatePosition();
+    }
+
+    updatePosition() {
+        if(this.remainingMovement > 0) {
+            const [property, change] = this.directionMovement[this.direction];
+            this[property] += change;
+            this.remainingMovement -= 1;
+        }
+    }
 }
