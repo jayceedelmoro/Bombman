f7b8c25063a37f02087ea4d5c776ab8e0aca1d2b
Carlo <jancarlodelmoro@gmail.com>
2023-02-20 22:23:00 +0800

    Fix: Added some comments

--- p2-js-game/GameObject.js
+++ p2-js-game/GameObject.js
@@ -22,6 +22,7 @@ class Person extends GameObjects {
         this.mainCharacter = config.mainCharacter || false;
         this.npc = config.npc || false;
 
+        //Specify the movement on the canvas
         this.directionMovement = {
             'up': ['locationY', -2],
             'down': ['locationY', 2],
@@ -34,18 +35,21 @@ class Person extends GameObjects {
         this.updatePosition();
         this.updateSpriteDirection(state);
 
+        //Moves Main Character if a movement direction is pressed
         if (this.mainCharacter && state.arrow) {
             this.direction = state.arrow;
         } else {
             this.direction = null;
         }
 
+        //Randomize NPC movement
         if (this.npc) {
             const randomDirection = Object.keys(this.directionMovement);
             this.direction = randomDirection[Math.floor(Math.random() * 4)];
         }
     }
 
+    //Update Character's Positions
     updatePosition() {
         if(this.direction !== null) {
             let [property, change] = this.directionMovement[this.direction];
@@ -54,10 +58,14 @@ class Person extends GameObjects {
     }
 
     updateSpriteDirection(state) {
+        //Adds animation and specify the direction the character is facing
         if(this.direction != null) {
             this.sprite.changeAnimation(`walk-${this.direction}`);
             this.lastFrame = this.sprite.currentAnimationFrame;
-        } else {
+        }
+        
+        //Pause at the last frame when the character stops moving
+        else {
             this.sprite.currentAnimationFrame = this.lastFrame;
         }
     }
--- p2-js-game/Sprite.js
+++ p2-js-game/Sprite.js
@@ -20,19 +20,17 @@ class Sprite {
 
         // Config Animation and Initial State
         this.animations = config.animations || {
-            'idle-up': [ [0,2] ],
-            'idle-down': [ [0,0] ],
-            'idle-left': [ [0,3] ],
-            'idle-right': [ [0,1] ],
             'walk-up': [ [0,2],[1,2],[0,2],[3,2] ],
             'walk-down': [ [0,0],[1,0],[0,0],[3,0] ],
             'walk-left': [ [0,3],[1,3],[0,3],[3,3] ],
             'walk-right': [ [0,1],[1,1],[0,1],[3,1] ]
         }
+
         this.currentAnimation = config.currentAnimation || 'walk-down';
         this.currentAnimationFrame = 0;
 
-        this.animationFrameSpeed = config.animationFrameSpeed || 4;
+        //How fast the frame changes into the next one
+        this.animationFrameSpeed = config.animationFrameSpeed || 6;
         this.animationFrameProgress = config.animationFrameProgress;
 
         //Reference Game Object
@@ -43,6 +41,7 @@ class Sprite {
         return this.animations[this.currentAnimation][this.currentAnimationFrame];
     }
 
+    //Changes the animation and character facing
     changeAnimation(directionKey) {
         if(this.currentAnimation !== directionKey){
             this.currentAnimation = directionKey;
@@ -51,6 +50,7 @@ class Sprite {
         }
     }
 
+    //Cycle through animation frames
     updateAnimation() {
         if(this.animationFrameProgress > 0) {
             this.animationFrameProgress -= 1;
@@ -60,15 +60,18 @@ class Sprite {
         this.animationFrameProgress = this.animationFrameSpeed;
         this.currentAnimationFrame +=1;
 
+        //Goes to the first animation frames
         if (this.frame === undefined) {
             this.currentAnimationFrame = 0;
         }
     }
 
+    //Draws Objects on canvas
     draw (ctx) {
         const locationX = this.gameObject.locationX - 5;
         const locationY = this.gameObject.locationY - 8;
 
+        //Specify which frame coordinates to show
         const [frameX, frameY] = this.frame;
 
         this.isShadowLoaded && ctx.drawImage(
@@ -77,11 +80,11 @@ class Sprite {
         );
         
         this.isLoaded && ctx.drawImage(
-                this.image,
-                frameX * 32, frameY * 32,
-                32, 32,
-                locationX, locationY,
-                32, 32
+                this.image, //img src
+                frameX * 32, frameY * 32, //starting point of the cut in the image
+                32, 32, // size of the cut
+                locationX, locationY, //location on the canvas
+                32, 32 //how big is the image
         )
 
         this.updateAnimation();
